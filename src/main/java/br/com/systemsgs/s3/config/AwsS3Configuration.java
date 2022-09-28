935aa08680bb1f87e7a156146f1ec6b568518bf1
GuilhermeJWT <guiromanno@gmail.com>
2022-09-28 13:04:01 -0300

    ADD config AwsCredentials e S3

---
+++ src/main/java/br/com/systemsgs/s3/config/AwsS3Configuration.java
@@ -0,0 +1,30 @@
+package br.com.systemsgs.s3.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsS3Configuration {
+
+    public AWSCredentials credentials() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials("accesskey", "secretkey");
+        return awsCredentials;
+    }
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials()))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+        return s3client;
+    }
+
+}
