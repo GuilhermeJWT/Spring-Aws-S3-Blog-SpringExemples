27fe616f220c3eacc4d11b0dda0a7e4fa34d8670
GuilhermeJWT <guiromanno@gmail.com>
2022-09-28 13:23:39 -0300

    ADD controller criaBucketS3

--- src/main/java/br/com/systemsgs/s3/controller/AwsS3Controller.java
+++ src/main/java/br/com/systemsgs/s3/controller/AwsS3Controller.java
@@ -1,6 +1,8 @@
 package br.com.systemsgs.s3.controller;
 
 import br.com.systemsgs.s3.service.AwsS3Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +15,10 @@ public class AwsS3Controller {
     public AwsS3Controller(AwsS3Service awsS3Service) {
         this.awsS3Service = awsS3Service;
     }
+
+    @PostMapping(value = "/{bucketName}")
+    public void createBucket(@PathVariable String bucketName){
+        awsS3Service.criaBucketS3(bucketName);
+    }
+
 }
